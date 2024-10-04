5670efa4a95ff2dea4f60f5d94583218b714a462
Heeyeon Lee <up2kid@gmail.com>
2024-10-04 19:46:18 +0900

    🚩 Providers 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
-import { Providers } from '@/oreurock-front/app/providers';
 import { Toaster } from 'sonner';
+import Providers from '@/app/providers';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -27,9 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Providers>
           {children}
           <Toaster
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,7 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <NextUIProvider>
-      <NextThemesProvider
-        attribute='class'
-        defaultTheme='light'
-        themes={['dark', 'light', 'modern']}
-      >
-        {children}
-      </NextThemesProvider>
-    </NextUIProvider>
-  );
+  return <NextUIProvider>{children}</NextUIProvider>;
 }
