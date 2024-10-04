3ef04e29cdba31a062794b00f4cea4a114d363f1
wheon06 <up2kid@gmail.com>
2024-10-04 16:59:05 +0900

    📝 Sonner 세팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { Providers } from '@/oreurock-front/app/providers';
+import { Toaster } from 'sonner';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -29,7 +30,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          {children}
+          <Toaster
+            toastOptions={{
+              classNames: { error: 'bg-red-500 text-white' },
+            }}
+          />
+        </Providers>
       </body>
     </html>
   );
