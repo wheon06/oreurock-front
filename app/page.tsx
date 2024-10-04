6351df69d26aab4524580ee5ecef66d78b68d189
wheon06 <up2kid@gmail.com>
2024-10-04 17:06:31 +0900

    🔥 미사용 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
       <div className='mx-auto min-h-screen max-w-4xl bg-white'>
         <CapsuleInfo />
         <div className='border-lightGray/30 my-[1%] w-full border-[1px]'></div>
-        {/*<PostList />*/}
       </div>
     </div>
   );
