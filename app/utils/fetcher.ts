e31b43595a16df5851fd668f36efb63fc6d1f333
Heeyeon Lee <up2kid@gmail.com>
2024-10-08 09:23:24 +0900

    💄 env 경로 수정

--- app/utils/fetcher.ts
+++ app/utils/fetcher.ts
@@ -1,6 +1,4 @@
-// const BASE_URL = process.env.BASE_URL;
-
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default async function fetcher(
   url: string,
